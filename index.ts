810f913192e91642dab8fe80227c0f5fd123ec8c
Luispe <38361000+lpbonomi@users.noreply.github.com>
2023-09-14 09:07:05 -0300

    reproduction

---
+++ index.ts
@@ -0,0 +1,55 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+// This works
+prisma.payment.create({
+  data: {
+    dummyModelId: 1,
+    statusId: 1,
+  },
+});
+
+// This works
+prisma.payment.create({
+  data: {
+    DummyModel: {
+      connectOrCreate: {
+        where: {
+          id: 1,
+        },
+        create: {
+          id: 1,
+          slug: "dummy",
+        },
+      },
+    },
+    Status: {
+      connectOrCreate: {
+        where: {
+          slug: "PENDING",
+        },
+        create: {
+          slug: "PENDING",
+        },
+      },
+    },
+  },
+});
+
+// This doesn't
+prisma.payment.create({
+  data: {
+    dummyModelId: 1,
+    Status: {
+      connectOrCreate: {
+        where: {
+          slug: "PENDING",
+        },
+        create: {
+          slug: "PENDING",
+        },
+      },
+    },
+  },
+});
